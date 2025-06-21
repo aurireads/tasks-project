97a4dbcbfb8606167aa24d7a4d82fee714fa9935
Mariane <comealongmari@gmail.com>
2025-06-21 17:08:22 -0300

    primeira commit

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    public function index()
+    {
+        return view("index");
+    }
+
+
+
+    public function create()
+    {
+        return view('create');
+    }
+}
---
+++ resources/views/index.blade.php
@@ -0,0 +1,17 @@
+@extends('layout')
+
+@section('main-content')
+<h4 class="pb-3">My Tasks</h4>
+</div>
+<div class="card">
+    <div class="card-header">
+        First Task Test
+    </div>
+    <div class="card-body">
+        <div class="card-text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis architecto possimus molestias provident.
+            Libero saepe tempora, nisi numquam cupiditate natus ullam quia beatae corrupti, adipisci eum blanditiis
+            voluptatem, pariatur perspiciatis.
+        </div>
+    </div>
+    @endsection
