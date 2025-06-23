1ca0d21b2b7076e48e0a48fcb82846c553159e5c
Mariane <comealongmari@gmail.com>
2025-06-22 23:18:23 -0300

    again

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../../css/app.css';
+import '../css/app.css';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
